243f38d7807a637b9824574603124c75fdb33303
isaccanedo <isaccanedo@gmail.com>
2021-11-09 10:13:04 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/controller/KafkaController.java
@@ -0,0 +1,18 @@
+package br.com.isaccanedo.controller;
+
+import br.com.isaccanedo.broker.producer.TopicProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final TopicProducer topicProducer;
+    @GetMapping(value = "/send")
+    public void send(){
+        topicProducer.send("Mensagem de teste enviada ao tópico");
+    }
+}
